d6fc1e14f15d214decf4ea45a70975acbea72ed6
Adam Timberlake <adam.timberlake@clearscore.com>
2023-01-23 12:52:03 +0000

    Added macro for Memo creation with variable args

--- src/lib.rs
+++ src/lib.rs
@@ -1,75 +1,74 @@
-// #[derive(Debug)]
-// pub struct Memo<Function, Return>
-// where
-//     Return: Clone,
-// {
-//     get_value: Function,
-//     pub value: Option<Return>,
-// }
-
-// macro_rules! create_memo_impl {
-//     ($($lowercase:ident => $uppercase:ident);*) => {
-//         impl<$($uppercase),*, Return> Memo<fn($($uppercase),*) -> Return, Return>
-//         where
-//             $($uppercase: Eq),*,
-//             Return: Clone,
-//         {
-//             pub fn new(f: fn($($uppercase),*) -> Return) -> Self {
-//                 Self {
-//                     get_value: f,
-//                     value: None,
-//                 }
-//             }
-
-//             pub fn run(&mut self, $($lowercase: $uppercase),*) -> Option<Return> {
-//                 match self.value {
-//                     Some(_) => self.value.to_owned(),
-//                     None => {
-//                         let value = Some((self.get_value)($($lowercase),*));
-//                         self.value = value;
-//                         self.value.to_owned()
-//                     }
-//                 }
-//             }
-//         }
-//     };
-// }
-
-// create_memo_impl!(arg1 => Arg1; arg2 => Arg2);
-// create_memo_impl!(arg1 => Arg1);
-
-struct Memo {}
-
-trait Memo1<A> {
-    fn new(a: A) -> i32;
+#[derive(Debug)]
+pub struct Memo<Function, Return>
+where
+    Return: Clone,
+{
+    get_value: Function,
+    pub value: Option<Return>,
 }
 
-trait Memo2<A, B> {
-    fn new(a: A, b: B) -> i32;
+macro_rules! create_memo_for {
+    ($trait_name:ident -> $($lowercase:ident => $uppercase:ident);*) => {
+        trait $trait_name<$($uppercase),*, Return> {
+            fn new(f: fn($($uppercase),*) -> Return) -> Self;
+            fn run(&mut self, $($lowercase: $uppercase),*) -> Option<Return>;
+        }
+
+        impl <$($uppercase),*, Return> $trait_name<$($uppercase),*, Return> for Memo<fn($($uppercase),*) -> Return, Return>
+        where
+            $($uppercase: Eq),*,
+            Return: Clone,
+        {
+            fn new(f: fn($($uppercase),*) -> Return) -> Self {
+                Self {
+                    get_value: f,
+                    value: None,
+                }
+            }
+
+            fn run(&mut self, $($lowercase: $uppercase),*) -> Option<Return> {
+                match self.value {
+                    Some(_) => self.value.to_owned(),
+                    None => {
+                        let value = Some((self.get_value)($($lowercase),*));
+                        self.value = value;
+                        self.value.to_owned()
+                    }
+                }
+            }
+        }
+    };
 }
 
-impl<A> Memo1<A> for Memo {
-    fn new(a: A) -> i32 {
-        5
-    }
-}
-
-impl<A, B> Memo2<A, B> for Memo {
-    fn new(a: A, b: B) -> i32 {
-        5
-    }
-}
+create_memo_for!(Memo1 -> arg1 => Arg1);
+create_memo_for!(Memo2 -> arg1 => Arg1; arg2 => Arg2);
+create_memo_for!(Memo3 -> arg1 => Arg1; arg2 => Arg2; arg3 => Arg3);
+create_memo_for!(Memo4 -> arg1 => Arg1; arg2 => Arg2; arg3 => Arg3; arg4 => Arg4);
+create_memo_for!(Memo5 -> arg1 => Arg1; arg2 => Arg2; arg3 => Arg3; arg4 => Arg4; arg5 => Arg5);
 
 #[cfg(test)]
 mod tests {
     use super::*;
 
     #[test]
-    fn it_caches_functions() {
-        let mut one = <Memo as Memo1<i32>>::new(1);
-        // assert_eq!(one.run(10), Some(10));
+    fn it_memoizes_functions_for_i32() {
+        // let mut f0 = <Memo<_, _> as Memo0<_>>::new(|| 10);
+        // assert_eq!(f0.run(10), Some(10));
 
-        let mut two = <Memo as Memo2<i32, i32>>::new(1, 2);
-        // assert_eq!(two.run(10, 10), Some(100));
+        let mut f1 = <Memo<_, _> as Memo1<_, _>>::new(|a| a);
+        assert_eq!(f1.run(10), Some(10));
+
+        let mut f2 = <Memo<_, _> as Memo2<_, _, _>>::new(|a, b| a * b);
+        assert_eq!(f2.run(10, 10), Some(100));
+
+        let mut f3 = <Memo<_, _> as Memo3<_, _, _, _>>::new(|a, b, c| a * b * c);
+        assert_eq!(f3.run(10, 10, 10), Some(1_000));
+
+        let mut f4 = <Memo<_, _> as Memo4<_, _, _, _, _>>::new(|a, b, c, d| a * b * c * d);
+        assert_eq!(f4.run(10, 10, 10, 10), Some(10_000));
+
+        let mut f5 =
+            <Memo<_, _> as Memo5<_, _, _, _, _, _>>::new(|a, b, c, d, e| a * b * c * d * e);
+        assert_eq!(f5.run(10, 10, 10, 10, 10), Some(100_000));
     }
 }
