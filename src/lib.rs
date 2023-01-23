5b4a4bc524045f7e0d0ec5a13f33484e26528b09
Adam Timberlake <adam.timberlake@clearscore.com>
2023-01-23 11:45:01 +0000

    Figuring out issue with conflicting definitions

--- src/lib.rs
+++ src/lib.rs
@@ -1,42 +1,64 @@
-#[derive(Debug)]
-pub struct Memo<Function, Return>
-where
-    Return: Clone,
-{
-    get_value: Function,
-    pub value: Option<Return>,
+// #[derive(Debug)]
+// pub struct Memo<Function, Return>
+// where
+//     Return: Clone,
+// {
+//     get_value: Function,
+//     pub value: Option<Return>,
+// }
+
+// macro_rules! create_memo_impl {
+//     ($($lowercase:ident => $uppercase:ident);*) => {
+//         impl<$($uppercase),*, Return> Memo<fn($($uppercase),*) -> Return, Return>
+//         where
+//             $($uppercase: Eq),*,
+//             Return: Clone,
+//         {
+//             pub fn new(f: fn($($uppercase),*) -> Return) -> Self {
+//                 Self {
+//                     get_value: f,
+//                     value: None,
+//                 }
+//             }
+
+//             pub fn run(&mut self, $($lowercase: $uppercase),*) -> Option<Return> {
+//                 match self.value {
+//                     Some(_) => self.value.to_owned(),
+//                     None => {
+//                         let value = Some((self.get_value)($($lowercase),*));
+//                         self.value = value;
+//                         self.value.to_owned()
+//                     }
+//                 }
+//             }
+//         }
+//     };
+// }
+
+// create_memo_impl!(arg1 => Arg1; arg2 => Arg2);
+// create_memo_impl!(arg1 => Arg1);
+
+struct Memo {}
+
+trait Memo1<A> {
+    fn new(a: A) -> i32;
 }
 
-macro_rules! create_memo_impl {
-    ($earg1:ident => $iarg1:ident; $earg2:ident => $iarg2:ident) => {
-        impl<$iarg1, $iarg2, Return> Memo<fn($iarg1, $iarg2) -> Return, Return>
-        where
-            $iarg1: Eq,
-            $iarg2: Eq,
-            Return: Clone,
-        {
-            pub fn new(f: fn($iarg1, $iarg2) -> Return) -> Self {
-                Self {
-                    get_value: f,
-                    value: None,
-                }
-            }
-
-            pub fn run(&mut self, $earg1: $iarg1, $earg2: $iarg2) -> Option<Return> {
-                match self.value {
-                    Some(_) => self.value.to_owned(),
-                    None => {
-                        let value = Some((self.get_value)($earg1, $earg2));
-                        self.value = value;
-                        self.value.to_owned()
-                    }
-                }
-            }
-        }
-    };
+trait Memo2<A, B> {
+    fn new(a: A, b: B) -> i32;
 }
 
-create_memo_impl!(arg1 => Arg1; arg2 => Arg2);
+impl<A> Memo1<A> for Memo {
+    fn new(a: A) -> i32 {
+        5
+    }
+}
+
+impl<A, B> Memo2<A, B> for Memo {
+    fn new(a: A, b: B) -> i32 {
+        5
+    }
+}
 
 #[cfg(test)]
 mod tests {
@@ -44,12 +66,10 @@ mod tests {
 
     #[test]
     fn it_caches_functions() {
-        fn multiply(a: i32, b: i32) -> isize {
-            println!("Called!");
-            return (a * b) as isize;
-        }
+        let mut one = <Memo as Memo1<i32>>::new(1);
+        // assert_eq!(one.run(10), Some(10));
 
-        let mut memoized_compute = Memo::new(multiply);
-        assert_eq!(memoized_compute.run(1, 10), Some(10));
+        let mut two = <Memo as Memo2<i32, i32>>::new(1, 2);
+        // assert_eq!(two.run(10, 10), Some(100));
     }
 }
